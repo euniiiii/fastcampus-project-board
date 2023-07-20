153f28e11c0671ee04803dd5660b773d60543236
euniiiii <hee9920@naver.com>
2023-07-20 12:56:25 +0900

    #1 - 도메인 클래스 설계: 주석 수정(게시글, 댓글)
    도메인 설계 주석 수정함

--- src/main/java/com/example/projectboard/domain/ArticleComment.java
+++ src/main/java/com/example/projectboard/domain/ArticleComment.java
@@ -3,8 +3,8 @@ package com.example.projectboard.domain;
 import java.time.LocalDateTime;
 
 public class ArticleComment {
-    private Long id; // 게시글 (ID)
-    private Article article;
+    private Long id;
+    private Article article; // 게시글 (ID)
     private String content; // 본문
 
     private LocalDateTime createdAt; // 생성일시
